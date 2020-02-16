a89fc09af78ef526cc265b37742e6b92a7cbfa28
Simon Kongm <simon.kong95@gmail.com>
2020-02-16 03:20:57 -0500

    reponsive screen container, digits, and transparent calculator

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const { app, BrowserWindow } = require('electron')
 
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({
       width: 800,
       height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      }
+      transparent: true,
+      frame: false
     })
   
     // and load the index.html of the app.
